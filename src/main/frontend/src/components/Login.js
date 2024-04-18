61daa62e7a80688e1b0831ef547dbb7fc9289fd2
asdf12q <rjsals5135@kyungmin.ac.kr>
2024-04-18 23:25:31 +0900

    login1

--- src/main/frontend/src/components/Login.js
+++ src/main/frontend/src/components/Login.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+
+
+
 
 function Example() {
     const [show, setShow] = useState(false);
@@ -18,16 +22,14 @@ function Example() {
                 <Modal.Header closeButton>
                     <Modal.Title>로그인</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <form>
-                        <label htmlFor="username">사용자 이름:</label><br/>
-                        <input type="text" id="username" name="username"/><br/>
-                        <label htmlFor="password">비밀번호:</label><br/>
-                        <input type="password" id="password" name="password"/><br/><br/>
-                        <input type="submit" value="로그인"/>
-                    </form>
-                </Modal.Body>
+                <Modal.Body closeButton>
+                    <Form>
 
+                        <input type="email" placeholder="Email" /><br/>
+                        <input type="password" placeholder="Password"/><br/>
+                        <input type="submit" value="로그인"/>
+                    </Form>
+                </Modal.Body>
             </Modal>
         </>
     );
